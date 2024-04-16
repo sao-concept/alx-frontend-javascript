c7ac68ca7320354fd2dd77844e7a0750f5e1b976
Azeez Saka <saoconcept1@gmail.com>
2024-04-16 07:43:23 -0700

    modified 100-weak.js file

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -4,5 +4,5 @@ export function queryAPI(endpoint) {
     weakMap.set(endpoint, 0);
   }
   weakMap.set(endpoint, weakMap.get(endpoint) + 1);
-  if (weakMap.get(endpoint) >= 5) throw Error("Endpoint load is high");
+  if (weakMap.get(endpoint) >= 5) throw Error('Endpoint load is high');
 }
