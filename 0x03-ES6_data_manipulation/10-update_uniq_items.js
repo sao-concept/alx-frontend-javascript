e8ca43addae36487a43d247834a3fb42a2db00c6
Azeez Saka <saoconcept1@gmail.com>
2024-04-16 07:41:27 -0700

    modified 10-update_uniq_items.js file

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,6 +1,5 @@
 export default function updateUniqueItems(groceries) {
-  if (Object.getPrototypeOf(groceries) !== Map.prototype)
-    throw TypeError("Cannot process");
+  if (Object.getPrototypeOf(groceries) !== Map.prototype) { throw TypeError('Cannot process'); }
 
   groceries.forEach((val, key) => {
     if (val === 1) {
